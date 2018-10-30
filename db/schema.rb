f4507574be313bdb8e6e0daf716af91fcf96edd4
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-29 21:51:04 -0300

    Add scaffold: task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_30_004258) do
+ActiveRecord::Schema.define(version: 2018_10_30_004858) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
