bb7682f6642df231dc3048d90c06e61f66b39d6b
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-29 21:57:48 -0300

    Add models relationships

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  has_many :todos
+  has_many :users, through: :todos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :todos
+  has_many :tasks, through: :todos
 end
