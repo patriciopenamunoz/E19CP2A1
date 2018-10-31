71635e8873efa3d9347ab904162b972c462da1b2
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-31 20:15:03 -0300

    Allow mark tasks as completed

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,12 @@
 class TodosController < ApplicationController
   def create
+    @todo = Todo.new(user_id: current_user.id,
+                     task_id: params[:task_id])
+    if @todo.save
+      redirect_to tasks_path, notice: 'Good job! Task marked as completed'
+    else
+      redirect_to tasks_path, alert: 'Oops, something went wrong...'
+    end
   end
 
   def destroy
