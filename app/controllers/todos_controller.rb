d08af4a12f2347a4f5ed4663c3f08195b0284684
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-31 19:09:49 -0300

    Add principal methods in todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,13 @@
 class TodosController < ApplicationController
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def todos_params
+    params.require(:todos).permit(:user_id, :task_id)
+  end
 end
