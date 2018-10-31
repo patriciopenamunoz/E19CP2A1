5ba9e1ee84ce93b8d2c6ffee73e3df28fa2a2eb6
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-31 19:52:33 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.delete_all
+20.times do |i|
+  task = Task.new
+  task.title = Faker::Hipster.sentence
+  task.description = Faker::Hipster.paragraph(2)
+  task.photo = "http://lorempixel.com/640/480/people/#{ i + 1 }"
+  task.save
+end
