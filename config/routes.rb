c4b2f52cf1c6ee1e7e804c0b4cf20d64b93e2cd8
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-31 19:06:47 -0300

    Define routes for todos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :tasks
+  resources :tasks do
+    resources :todos, only: %i[create destroy]
+  end
   devise_for :users
+  resources :users, only: [] do
+    resources :todos, only: %i[create destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
