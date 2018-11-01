af91ccf2f0dbe6cf22bc51a065d3da10a3437a46
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-11-01 14:51:22 -0300

    Adapt bootstrap style to devise session and registration forms.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :tasks do
     resources :todos, only: %i[create destroy]
   end
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :users, only: [] do
     resources :todos, only: %i[create destroy]
   end
